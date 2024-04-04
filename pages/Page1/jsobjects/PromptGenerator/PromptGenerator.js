47ef145cd7325d02de1a73385adede82fd585f54
dipanjan <dipanjan@rudderlabs.com>
2024-04-04 13:40:07 +0000

    Updated prompt

--- pages/Page1/jsobjects/PromptGenerator/PromptGenerator.js
+++ pages/Page1/jsobjects/PromptGenerator/PromptGenerator.js
@@ -2,7 +2,7 @@ export default {
 	generatePrompt () {
 		//	write code here
 		//	this.myVar1 = [1,2,3]
-				var prompt = 'Following are the column names for a Snowflake table ' + Select1.selectedOptionValue  + '.' + Select2.selectedOptionValue + '.' + Select3.selectedOptionValue + ' : ' + MultiSelect1.selectedOptionValues + '. ' + Input1.text + '. Print only the SQL.';
+				var prompt = 'Following are the column names for a Snowflake table ' + Select1.selectedOptionValue  + '.' + Select2.selectedOptionValue + '.' + Select3.selectedOptionValue + ' : ' + MultiSelect1.selectedOptionValues + '. ' + Input1.text + '. Print only the SQL. Do not print any markdown';
 		Input2.setValue(prompt);
 
 	},
